7f8322dd05ec0c59ce9ce7ddc9f6994be4a55439
Reuben Rapose <reuben.rapose@gmail.com>
2023-03-29 16:42:49 +0530

    Removed unnecessary dom script

--- components/showcase/MagicalHoverEffect.tsx
+++ components/showcase/MagicalHoverEffect.tsx
@@ -37,16 +37,6 @@ const cards = [
 
 export const MagicalHoverEffect = () => {
   useEffect(() => {
-    const script = document.createElement('script')
-    script.src = 'https://kit.fontawesome.com/1ee8f271b9.js'
-    document.body.appendChild(script)
-
-    return () => {
-      document.body.removeChild(script)
-    }
-  }, [])
-
-  useEffect(() => {
     document.getElementById('cards').onmousemove = (e) => {
       for (const card of document.getElementsByClassName('card')) {
         const rect = card.getBoundingClientRect(),
