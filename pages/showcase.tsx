843aebaa0e94518c1677eb6f42994f12a6636d0f
Reuben Rapose <reuben.rapose@gmail.com>
2023-04-03 15:36:20 +0530

    Removed MorphingText

--- pages/showcase.tsx
+++ pages/showcase.tsx
@@ -47,7 +47,6 @@ export default function Showcase() {
             <HackerEffectText />
             <MagicalHoverEffect />
             <InfiniteScrollHead />
-            <MorphingText />
             <SparkleButton />
             <AuroraText />
           </div>
